713ecf1a01d6744c00dd238251c01cde7b035c1a
seba-1511 <seba-1511@hotmail.com>
2015-02-01 11:22:41 -0800

    Added default files for the SRS.

--- prototype/app/scripts/app.js
+++ prototype/app/scripts/app.js
@@ -9,35 +9,35 @@
  * Main module of the application.
  */
 angular
-  .module('prototypeApp', [
-    'ngAnimate',
-    'ngAria',
-    'ngCookies',
-    'ngMessages',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
-      .when('/Songs', {
-        templateUrl: 'views/songs.html',
-        controller: 'SongsCtrl'
-      })
-      .when('/Song', {
-        templateUrl: 'views/song.html',
-        controller: 'SongCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+    .module('prototypeApp', [
+        'ngAnimate',
+        'ngAria',
+        'ngCookies',
+        'ngMessages',
+        'ngResource',
+        'ngRoute',
+        'ngSanitize',
+        // 'ngTouch'
+    ])
+    .config(function($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl'
+            })
+            .when('/about', {
+                templateUrl: 'views/about.html',
+                controller: 'AboutCtrl'
+            })
+            .when('/Songs', {
+                templateUrl: 'views/songs.html',
+                controller: 'SongsCtrl'
+            })
+            .when('/Song', {
+                templateUrl: 'views/song.html',
+                controller: 'SongCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    });
